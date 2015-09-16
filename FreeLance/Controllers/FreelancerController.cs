386b4cd9d8b3a892ccbbde3b065b42393ca4e144
AndrewA <aiandrianov@gmail.com>
2015-09-16 22:35:06 +0300

    tabfix

--- FreeLance/Controllers/FreelancerController.cs
+++ FreeLance/Controllers/FreelancerController.cs
@@ -10,59 +10,59 @@ using Microsoft.AspNet.Identity;
 namespace FreeLance.Controllers
 {
 	[Authorize(Roles = "Admin, Freelancer")]
-    public class FreelancerController : Controller
-    {
-        private ApplicationDbContext db = new ApplicationDbContext();
+	public class FreelancerController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
 
-        public class HomeView
-        {
-            public List<ContractModels> Contracts { get; set; }
-            public List<ProblemModels> Problems { get; set; }
-        }
+		public class HomeView
+		{
+			public List<ContractModels> Contracts { get; set; }
+			public List<ProblemModels> Problems { get; set; }
+		}
 
-        public ActionResult Index()
-        {
-            return RedirectToAction("Home");
-        }
+		public ActionResult Index()
+		{
+			return RedirectToAction("Home");
+		}
 
-        // GET: Freelancer
-        public ActionResult Home()
-        {
-            var viewModel = new HomeView
-            {
-                Contracts = db.ContractModels.ToList(),
-                Problems = db.ProblemModels.ToList()
-            };
-            return View(viewModel);
-        }
+		// GET: Freelancer
+		public ActionResult Home()
+		{
+			var viewModel = new HomeView
+			{
+				Contracts = db.ContractModels.ToList(),
+				Problems = db.ProblemModels.ToList()
+			};
+			return View(viewModel);
+		}
 
-        public ActionResult Archive()
-        {
-            return View(db.ContractModels.ToList());
-        }
+		public ActionResult Archive()
+		{
+			return View(db.ContractModels.ToList());
+		}
 
-        public ActionResult Contract(int id)
-        {
-            return View(db.ContractModels.Find(id));
-        }
+		public ActionResult Contract(int id)
+		{
+			return View(db.ContractModels.Find(id));
+		}
 
 		public class ProblemView
 		{
 			public ProblemModels ProblemModels { get; set; }
 			public bool IsSubscibed { get; set; }
-        }
+		}
 
-        public ActionResult Problem(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+		public ActionResult Problem(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
 
-            ProblemModels problemModels = db.ProblemModels.Find(id);
-            if (problemModels == null)
-            {
-                return HttpNotFound();
+			ProblemModels problemModels = db.ProblemModels.Find(id);
+			if (problemModels == null)
+			{
+				return HttpNotFound();
 			}
 			string userId = User.Identity.GetUserId();
 			SubscriptionModels[] subscriptions = db.SubscriptionModels.Where(sub => sub.Freelancer.Id == userId
@@ -74,12 +74,12 @@ namespace FreeLance.Controllers
 				IsSubscibed = subscription != null
 			};
 			return View(view);
-        }
+		}
 
-        public ViewResult OpenProblems()
-        {
-            ProblemModels[] openProblems = db.ProblemModels.Where(x => x.Status == 0).ToArray();            
-            return View(openProblems);
-        }
-    }
+		public ViewResult OpenProblems()
+		{
+			ProblemModels[] openProblems = db.ProblemModels.Where(x => x.Status == 0).ToArray();
+			return View(openProblems);
+		}
+	}
 }
