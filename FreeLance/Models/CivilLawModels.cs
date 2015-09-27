d11bf215133c00370a0c33f11e71eeef96b34723
ava-katushka <vtknann@gmail.com>
2015-09-27 21:47:05 +0300

    Создала ГПХ

---
+++ FreeLance/Models/CivilLawModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FreeLance.Models
+{
+    public class EntityModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+
+    public class CivilLawContract
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public EntityModel Entity;
+    }
+}
