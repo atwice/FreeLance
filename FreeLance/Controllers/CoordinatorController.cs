afd4a6f3c10831cd9e3b38f86a2e4bd3a4291488
Nikolay Kochetov <kochetovnicolai@gmail.com>
2015-09-27 23:08:41 +0300

    модель пакета документов

--- FreeLance/Controllers/CoordinatorController.cs
+++ FreeLance/Controllers/CoordinatorController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using FreeLance.Models;
 
 namespace FreeLance.Controllers
 {
---
+++ FreeLance/Models/DocumentPackageModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FreeLance.Models {
+	public class DocumentPackageModels {
+
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public virtual ApplicationUser User { get; set; }
+
+		[Required]
+		public bool IsApproved { get; set; }
+
+		// Добавлять документы по ходу создания их моделей
+	}
+}
