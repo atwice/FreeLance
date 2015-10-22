9f03fb7061a8ffa1de0684029dbca01c4e4a7aa8
Сандрикова Мария <mashasandrikova@gmail.com>
2015-10-22 17:17:49 +0300

    Отправка email и интерфейс EmailBuilder

---
+++ FreeLance/Code/EmailManager.cs
@@ -0,0 +1,14 @@
+﻿using Postal;
+
+namespace FreeLance.Code
+{
+	public class EmailManager
+	{
+		public static void Send(EmailBuilder builder)
+		{
+			Email email = builder.PrepareEmail();
+			email.Send();
+		}
+	}
+	
+}
