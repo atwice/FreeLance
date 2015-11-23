22e97818c33dc4fb076cf3ad4d828fb57ed76c42
Andrey Andrianov <aiandrianov@gmail.com>
2015-11-23 18:37:16 +0300

    Стащил tinymce к нам на сайт.
    Настройки tinymce: не отображаем меню, не отображаем статусбар.

--- FreeLance/App_Start/BundleConfig.cs
+++ FreeLance/App_Start/BundleConfig.cs
@@ -3,30 +3,29 @@ using System.Web.Optimization;
 
 namespace FreeLance
 {
-    public class BundleConfig
-    {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+						"~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+						"~/Scripts/bootstrap.js",
+						"~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/Site.css"
-                      ));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+						"~/Content/bootstrap.css",
+						"~/Content/Site.css"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jquery-signalR").Include(
 						"~/Scripts/jquery.signalR-2.1.2.js"));
@@ -44,6 +43,12 @@ namespace FreeLance
 
 			bundles.Add(new StyleBundle("~/bundles/chat/css").Include(
 						"~/Content/chat.css"));
+
+			bundles.Add(new ScriptBundle("~/bundles/tinymce").
+				Include("~/Scripts/tinymce/tinymce.min.js").
+				Include("~/Scripts/tinymce.init.js").
+				IncludeDirectory("~/Scripts/tinymce/", "*.js")
+			);
 		}
 	}
 }
---
+++ FreeLance/Scripts/tinymce.init.js
@@ -0,0 +1,7 @@
+﻿tinymce.init({
+	selector: "textarea.rich-text-editor",
+	menubar: false,
+	statusbar: false,
+	height: 300,
+	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons"
+ });
